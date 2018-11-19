309eaba542d387ecb68acb20cf8d6b7e9a1fcd3f
Chia <dustinprobbins@yahoo.com>
2018-11-19 04:26:05 -0500

    Cleaned up input validator somewhat, added discriminant calculation to main().

--- src/main/main.c
+++ src/main/main.c
@@ -3,9 +3,9 @@
 #include <gmp.h>
 
 int main(int argc, char *argv[]){
+	mpf_set_default_prec(5);
 
 	double * toLoad = malloc(sizeof(double) * 3);
-	int debugIndicator;
 	char * inBuffer = malloc(sizeof(char) * 256);
 	
 	printf("Note: Program will continue execution until three valid inputs are entered.\n");
@@ -14,47 +14,52 @@ int main(int argc, char *argv[]){
 	do{
 		printf("Please enter a number for value a: ");
 		fgets(inBuffer, 256, stdin);
-		debugIndicator = sscanf(inBuffer, "%lf", toLoad);
 	}
-	while(debugIndicator != 1);
+	while(sscanf(inBuffer, "%lf", toLoad) != 1);
 
-	debugIndicator = 0;
-	
 	do{
 		printf("Please enter a number for value b: ");
 		fgets(inBuffer, 256, stdin);
-		debugIndicator = sscanf(inBuffer, "%lf", toLoad + sizeof(double));
 	}
-	while(debugIndicator != 1);
-
-	debugIndicator = 0;
+	while(sscanf(inBuffer, "%lf", toLoad + sizeof(double)) != 1);
 	
 	do{
 		printf("Please enter a number for value c: ");
 		fgets(inBuffer, 256, stdin);
-		debugIndicator = sscanf(inBuffer, "%lf", toLoad + 2 * sizeof(double));
 	}
-	while(debugIndicator != 1);
+	while(sscanf(inBuffer, "%lf", toLoad + 2 * sizeof(double)) != 1);
 
-	mpf_set_default_prec(5);
 
 	double i_a = *toLoad, i_b = *(toLoad + sizeof(double)), i_c = *(toLoad + 2 * sizeof(double)); // Will be parsed from input
 	mpf_t a, b, c; // Will be worked with throughout
+	mpf_t discriminant, bsq, fac, num; //The variables that will store the discriminant,
+	//b squared, and 4*ac, respectively, and the variable that will let us multiply a*c 
+	// by four to get 4*ac
 
-	// mpf_set_d(num, -4);
+	mpf_init_set_d(a,i_a); 
+	mpf_init_set_d(b,i_b);
+	mpf_init_set_d(c,i_c);
 
-	mpf_init(a);
-	mpf_init(b);
-	mpf_init(c);
+	gmp_printf("a: %Ff\nb: %Ff\nc: %Ff\n",a,b,c);
 
-	mpf_set_d(a,i_a); 
-	// gmp_printf("a: %Ff\n", a); 
-	mpf_set_d(b,i_b);
-	// gmp_printf("b: %Ff\n", b); 
-	mpf_set_d(c,i_c);
-	// gmp_printf("c: %Ff\n", c); 
+	mpf_init(discriminant); // Initiating values to get discriminant
+	mpf_init(bsq);
+	mpf_init(fac);
+	mpf_init_set_d(num, 4.0);
 
-	gmp_printf("a: %Ff\nb: %Ff\nc: %Ff",a,b,c);
+	mpf_mul(fac,a,c); //Starting to get 4*ac
+	mpf_mul(fac,num,fac); // There it is
+
+	mpf_mul(bsq,b,b); // b-squared
+
+	mpf_sub(discriminant,bsq,fac); // Get discriminant
+
+	gmp_printf("Discriminant: %Ff\n",discriminant);
+
+	mpf_clear(bsq); // Don't need these, now
+	mpf_clear(fac);
+
+	// To be resumed ***************************
 
 	mpf_clear(a);
 	mpf_clear(b);
--- src/modules/runtimeLoad.c
+++ src/modules/runtimeLoad.c
@@ -4,7 +4,6 @@
 void runtimeLoad(double * toLoad)
 {
 	//printf("%p", toLoad);
-	int debugIndicator;
 	char * inBuffer = malloc(sizeof(char) * 256);
 	
 	printf("Note: Program will continue execution until three valid inputs are entered.\n");
@@ -13,27 +12,20 @@ void runtimeLoad(double * toLoad)
 	do{
 		printf("Please enter a number for value a: ");
 		fgets(inBuffer, 256, stdin);
-		debugIndicator = sscanf(inBuffer, "%lf", toLoad);
 	}
-	while(debugIndicator != 1);
+	while(sscanf(inBuffer, "%lf", toLoad) != 1);
 
-	debugIndicator = 0;
-	
 	do{
 		printf("Please enter a number for value b: ");
 		fgets(inBuffer, 256, stdin);
-		debugIndicator = sscanf(inBuffer, "%lf", toLoad + sizeof(double));
 	}
-	while(debugIndicator != 1);
+	while(sscanf(inBuffer, "%lf", toLoad + sizeof(double)) != 1);
 
-	debugIndicator = 0;
-	
 	do{
 		printf("Please enter a number for value c: ");
 		fgets(inBuffer, 256, stdin);
-		debugIndicator = sscanf(inBuffer, "%lf", toLoad + 2 * sizeof(double));
 	}
-	while(debugIndicator != 1);
+	while(sscanf(inBuffer, "%lf", toLoad + 2 * sizeof(double)) != 1);
 
 	printf("%f", *toLoad);
 	printf("%f", *(toLoad + sizeof(double)));
