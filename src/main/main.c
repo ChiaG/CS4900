7cb82481e531f46f6b86463dead327cfd43d2915
Chia <dustinprobbins@yahoo.com>
2018-11-19 00:12:54 -0500

    Fixed use of gmp_printf() in main.

--- src/main/main.c
+++ src/main/main.c
@@ -4,35 +4,36 @@
 int main(int argc, char *argv[]){
 	mpf_set_default_prec(5);
 
-	double i_a, i_b, i_c; // Will be parsed from input
+	float i_a, i_b, i_c; // Will be parsed from input
 	mpf_t a, b, c; // Will be worked with throughout
 
 	// mpf_set_d(num, -4);
 
 	if(argc<4){
-
 	    printf("Please enter 3 floating-point numbers with a newline after each:\n");
-	        scanf("%lf",&i_a);
-	        scanf("%lf",&i_b);
-	        scanf("%lf",&i_c);
+        scanf("%f",&i_a);
+        scanf("%f",&i_b);
+        scanf("%f",&i_c);
 	} 
 
 	else{
-	    sscanf(argv[1], "%lf", &i_a);
-	    sscanf(argv[2], "%lf", &i_b);
-	    sscanf(argv[3], "%lf", &i_c);
+	    sscanf(argv[1], "%f", &i_a);
+	    sscanf(argv[2], "%f", &i_b);
+	    sscanf(argv[3], "%f", &i_c);
 	}
 
-	printf("%f %f %f\n\n",i_a,i_b,i_c); // Prints doubles to ensure accuracy
-	mpf_init_set_d(a,i_a); // Initiates and sets a
-	mpf_set_d(a,i_a);
-	gmp_printf("a: %F\n", a); // Prints a, showing that it was not assigned correctly
-	mpf_init_set_d(b,i_b);
-	gmp_printf("b: %F\n", b); // Prints b, showing that it was not assigned correctly
-	mpf_init_set_d(c,i_c);
-	gmp_printf("c: %F\n", c); // Prints c, showing that it was not assigned correctly
+	mpf_init(a);
+	mpf_init(b);
+	mpf_init(c);
 
-	// gmp_printf("a: %F\n b: %F\n c: %F",a,b,c);
+	mpf_set_d(a,i_a); 
+	// gmp_printf("a: %Ff\n", a); 
+	mpf_set_d(b,i_b);
+	// gmp_printf("b: %Ff\n", b); 
+	mpf_set_d(c,i_c);
+	// gmp_printf("c: %Ff\n", c); 
+
+	gmp_printf("a: %Ff\n b: %Ff\n c: %Ff",a,b,c);
 
 	mpf_clear(a);
 	mpf_clear(b);
