108919774d87161ce39e31c4ef1f6fc2b37f8d12
Chia <dustinprobbins@yahoo.com>
2018-11-24 02:57:32 -0500

    Made some formatting changes to set code more in line with Linux kernal coding style.

--- src/main/main.c
+++ src/main/main.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>			
 #include <gmp.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 	mpf_set_default_prec(6);
 
-	double * toLoad = malloc(sizeof(double) * 3);
-	char * inBuffer = malloc(sizeof(char) * 256);
+	double *toLoad = malloc(sizeof(double) * 3);
+	char *inBuffer = malloc(sizeof(char) * 256);
 	
 	printf("Note: Program will continue execution until three valid inputs are entered.\n");
 	printf("Note: Program will disregard any numerical entries after the first for a given value.\n");
@@ -22,40 +23,52 @@ int main(int argc, char *argv[]){
 		printf("Please enter a number for value b: ");
 		fgets(inBuffer, 256, stdin);
 	}
-	while(sscanf(inBuffer, "%lf", toLoad + sizeof(double)) != 1 || *(toLoad + sizeof(double)) == 0);
+	while(sscanf(inBuffer, "%lf", toLoad + sizeof(double)) != 1 
+		|| *(toLoad + sizeof(double)) == 0);
 	
 	do{
 		printf("Please enter a number for value c: ");
 		fgets(inBuffer, 256, stdin);
 	}
-	while(sscanf(inBuffer, "%lf", toLoad + 2 * sizeof(double)) != 1 || *(toLoad + 2 * sizeof(double)) == 0);
+	while(sscanf(inBuffer, "%lf", toLoad + 2 * sizeof(double)) != 1 
+		|| *(toLoad + 2 * sizeof(double)) == 0);
 
 
-	double i_a = *toLoad, i_b = *(toLoad + sizeof(double)), i_c = *(toLoad + 2 * sizeof(double)); // Will be parsed from input
-	mpf_t a, b, c; // Will be worked with throughout
-	mpf_t discriminant, bsq, fac, num; //The variables that will store the discriminant,
+	double i_a = *toLoad, i_b = *(toLoad + sizeof(double));
+	double i_c = *(toLoad + 2 * sizeof(double)); // Will be parsed from input
+	
+	// a, b, and c values for quadratic formula
+	mpf_t a;
+	mpf_t b;
+	mpf_t c; 
+	
+	//The variables that will store the discriminant,
 	//b squared, and 4*ac, respectively, and the variable that will let us multiply a*c 
 	// by four to get 4*ac
+	mpf_t discriminant;
+	mpf_t bsq; 
+	mpf_t fac;
+	mpf_t num;
 
-	mpf_init_set_d(a,i_a); 
-	mpf_init_set_d(b,i_b);
-	mpf_init_set_d(c,i_c);
+	mpf_init_set_d(a, i_a); 
+	mpf_init_set_d(b, i_b);
+	mpf_init_set_d(c, i_c);
 
-	// gmp_printf("\na: %Ff\nb: %Ff\nc: %Ff\n",a,b,c);
+	// gmp_printf("\na: %Ff\nb: %Ff\nc: %Ff\n", a, b, c);
 
 	mpf_init(discriminant); // Initiating values to get discriminant
 	mpf_init(bsq);
 	mpf_init(fac);
 	mpf_init_set_d(num, 4.0);
 
-	mpf_mul(fac,a,c); //Starting to get 4*ac
-	mpf_mul(fac,num,fac); // There it is
+	mpf_mul(fac, a, c); //Starting to get 4*ac
+	mpf_mul(fac, num, fac); // There it is
 
-	mpf_mul(bsq,b,b); // b-squared
+	mpf_mul(bsq, b, b); // b-squared
 
-	mpf_sub(discriminant,bsq,fac); // Get discriminant
+	mpf_sub(discriminant, bsq, fac); // Get discriminant
 
-	gmp_printf("\nDiscriminant: %Ff\n",discriminant);
+	gmp_printf("\nDiscriminant: %Ff\n", discriminant);
 
 	mpf_clear(bsq); // Don't need these, now
 	mpf_clear(fac);
@@ -88,11 +101,11 @@ int main(int argc, char *argv[]){
 
 	mpf_neg(neg_b, b);
 
-	if(0 > mpf_cmp(discriminant, zeroCmp)){ //comparewith 0 to see if negative
+	if(0 > mpf_cmp(discriminant, zeroCmp)) { //comparewith 0 to see if negative
 		printf("%s\n","There are no real solutions");
 	}
 
-	else if(mpf_cmp(discriminant, zeroCmp) == 0){		//-b/2a if discriminant is 0
+	else if(mpf_cmp(discriminant, zeroCmp) == 0) {		//-b/2a if discriminant is 0
 		mpf_div(rootOne, neg_b, denom);
 		gmp_printf("\nAll Roots: %Ff\n", rootOne);		//rootOne = rootTwo etc
 		printf("Accurate to at least 6 decimal places.\n");
@@ -118,11 +131,11 @@ int main(int argc, char *argv[]){
 		//I would have done the top and compared to print instead.
 		********************************/
 
-		if(0 > mpf_cmp(b, zeroCmp)){  //if b is negative then do -b+sqrt/2a
+		if(0 > mpf_cmp(b, zeroCmp)) {  //if b is negative then do -b+sqrt/2a
 			mpf_add(numerator, neg_b,sqrt);
 			mpf_div(rootOne, numerator, denom);
 
-			if(mpf_cmp(rootOne, c)>0){				//if root one is greater than c then c/rootOne is rootTwo
+			if(mpf_cmp(rootOne, c) > 0) {				//if root one is greater than c then c/rootOne is rootTwo
 				mpf_div(rootTwo, c, rootOne);
 			}
 			else{
@@ -135,7 +148,7 @@ int main(int argc, char *argv[]){
 			mpf_sub(numerator, neg_b,sqrt);
 			mpf_div(rootOne, numerator, denom);
 
-			if(mpf_cmp(rootOne, c)>0){				//if root one is greater than c then c/rootOne is rootTwo
+			if(mpf_cmp(rootOne, c) > 0) {				//if root one is greater than c then c/rootOne is rootTwo
 				mpf_div(rootTwo, c, rootOne);
 			}
 			else{
